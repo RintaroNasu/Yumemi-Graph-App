f395323053f094375f9d891dee7e6dfcbc240bc0
RintaroNasu <rinta022823@gmail.com>
2024-11-12 01:31:32 +0900

    fix:ボタン周りをレスポンシブ対応へ

--- src/components/molecules/PopulationButtonGroup.tsx
+++ src/components/molecules/PopulationButtonGroup.tsx
@@ -8,7 +8,7 @@ type Props = {
 export const PopulationButtonGroup = (props: Props) => {
   const { dataTypeIndex, setDataTypeIndex } = props;
   return (
-    <div className="flex justify-center gap-5">
+    <div className="flex flex-col sm:flex-row justify-center items-center gap-5 p-5">
       <PopulationButton label="総人口" isActive={dataTypeIndex === 0} onClick={() => setDataTypeIndex(0)} />
       <PopulationButton label="年少人口" isActive={dataTypeIndex === 1} onClick={() => setDataTypeIndex(1)} />
       <PopulationButton label="生産年齢人口" isActive={dataTypeIndex === 2} onClick={() => setDataTypeIndex(2)} />
