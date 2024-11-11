16b285158d4f4fb36ff88a9ed9c6a73fb3e1d5bf
RintaroNasu <rinta022823@gmail.com>
2024-11-11 23:14:36 +0900

    feat:総人口のグラフ表示

--- src/components/PrefecturesCheckList.tsx
+++ src/components/PrefecturesCheckList.tsx
@@ -1,16 +1,25 @@
 "use client";
 
-import { getPrefectures } from "@/utils/getResasApi";
+import { getPopulationPerYear, getPrefectures } from "@/utils/getResasApi";
 import { useEffect, useState } from "react";
+import { CartesianGrid, Legend, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts";
 
 type Prefecture = {
   prefCode: number;
   prefName: string;
 };
 
+type PopulationData = {
+  year: number;
+  [prefCode: string]: number;
+};
+
+const INITIAL_YEARS = Array.from({ length: 18 }, (_, i) => ({ year: 1960 + i * 5 }));
+
 export const PrefecturesCheckList = () => {
   const [prefectures, setPrefectures] = useState<Prefecture[]>([]);
   const [selectedPrefectures, setSelectedPrefectures] = useState<Set<number>>(new Set());
+  const [populationData, setPopulationData] = useState<PopulationData[]>([]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -36,6 +45,32 @@ export const PrefecturesCheckList = () => {
     });
   };
 
+  useEffect(() => {
+    const fetchPopulationData = async () => {
+      const updatedPopulationData: PopulationData[] = [...INITIAL_YEARS];
+
+      try {
+        for (const prefCode of selectedPrefectures) {
+          const population = await getPopulationPerYear(prefCode);
+          population.result.data[0].data.forEach((item: { year: number; value: number }, index: number) => {
+            if (!updatedPopulationData[index]) {
+              updatedPopulationData[index] = { year: item.year };
+            }
+            updatedPopulationData[index][prefCode] = item.value;
+          });
+        }
+        setPopulationData(updatedPopulationData);
+      } catch (error) {
+        console.error("Error fetching population data:", error);
+      }
+    };
+    if (selectedPrefectures.size > 0) {
+      fetchPopulationData();
+    } else {
+      setPopulationData(INITIAL_YEARS);
+    }
+  }, [selectedPrefectures]);
+
   return (
     <>
       <div className="flex flex-wrap max-w-7xl mx-auto mt-4  rounded-lg bg-gray-50 p-5 shadow-lg">
@@ -46,6 +81,21 @@ export const PrefecturesCheckList = () => {
             <label htmlFor={`pref-${prefecture.prefCode}`}>{prefecture.prefName}</label>
           </div>
         ))}
+      </div>
+      <div className="mt-5">
+        <div className="w-full text-center text-2xl font-semibold mb-4">総人口推移</div>
+        <ResponsiveContainer width="100%" height={400}>
+          <LineChart data={populationData} margin={{ top: 20, right: 30, left: 20, bottom: 20 }}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="year" label={{ value: "年", position: "insideBottomRight", offset: -10 }} />
+            <YAxis label={{ value: "人口数", angle: -90, position: "insideLeft" }} />
+            <Tooltip />
+            <Legend />
+            {Array.from(selectedPrefectures).map((prefCode) => (
+              <Line key={prefCode} type="monotone" dataKey={prefCode} name={prefectures.find((pref) => pref.prefCode === prefCode)?.prefName} dot={false} />
+            ))}
+          </LineChart>
+        </ResponsiveContainer>
       </div>
     </>
   );
