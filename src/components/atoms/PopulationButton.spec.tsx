c2fe66697eaed42906c886335df6c8f7fe3efc52
RintaroNasu <rinta022823@gmail.com>
2024-11-14 16:17:04 +0900

    refactor:空白削除

--- src/components/atoms/PopulationButton.spec.tsx
+++ src/components/atoms/PopulationButton.spec.tsx
@@ -1,25 +1,25 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import { PopulationButton } from "./PopulationButton";
 
-describe("PopulationButton コンポーネント", () => {
-  it("label が正しく表示される", () => {
+describe("PopulationButtonコンポーネント", () => {
+  it("labelが正しく表示される", () => {
     render(<PopulationButton label="人口ボタン" isActive={false} onClick={() => {}} />);
     expect(screen.getByText("人口ボタン")).toBeInTheDocument();
   });
 
-  it("isActive が true の場合、アクティブなスタイルが適用される", () => {
+  it("isActiveがtrueの場合、アクティブなスタイルが適用される", () => {
     render(<PopulationButton label="人口ボタン" isActive={true} onClick={() => {}} />);
     const button = screen.getByRole("button");
     expect(button).toHaveClass("bg-[#2d716d]");
   });
 
-  it("isActive が false の場合、非アクティブなスタイルが適用される", () => {
+  it("isActiveがfalseの場合、非アクティブなスタイルが適用される", () => {
     render(<PopulationButton label="人口ボタン" isActive={false} onClick={() => {}} />);
     const button = screen.getByRole("button");
     expect(button).toHaveClass("bg-[rgba(0,164,150,1)]");
   });
 
-  it("onClick 関数がクリック時に呼び出される", () => {
+  it("onClick関数がクリック時に呼び出される", () => {
     const handleClick = jest.fn();
     render(<PopulationButton label="人口ボタン" isActive={false} onClick={handleClick} />);
     const button = screen.getByRole("button");
